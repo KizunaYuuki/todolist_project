abde4d60c5d4a4c44db10b6a9ca3072c1f2c24af
Kizuna Yuuki <kizunayuuki@Kizunas-iMac.local>
2022-11-04 09:16:06 +0700

    Fri Nov  4 09:15:46 +07 2022

---
+++ backend/app/config/server.config.js
@@ -0,0 +1,10 @@
+const config = {
+    app: {
+        port: process.env.PORT || 3000
+    },
+    db: {
+        // Connection string
+        uri: process.env.MONGODB_URI || "mongodb+srv://mongo:mongo@cluster0.5dw7dn2.mongodb.net/?retryWrites=true&w=majority"
+    }
+};
+module.exports = config;
